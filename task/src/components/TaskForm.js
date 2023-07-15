0d92732022b78151c05e9388612d417abdf8f1a8
Kesav-21 <cgkesav@gmail.com>
2023-07-15 13:31:14 +0530

    form completed

--- task/src/components/TaskForm.js
+++ task/src/components/TaskForm.js
@@ -6,9 +6,35 @@ const TaskForm = () => {
   const [due,setDue]=useState("")
   const [priority,setPriority]=useState("")
   const [label,setLabel]=useState("")
+  const [error,setError]=useState(null)
 
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+    const task={title,due,priority,label}
+
+    const response=await fetch('/api/tasks',{
+      method:'POST',
+      body:JSON.stringify(task),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    const json=await response.json()
+
+    if(!response.ok){
+      setError(json.error)
+    }
+    if(response.ok){
+      setTitle('')
+      setDue('')
+      setPriority('')
+      setLabel('')
+      setError(null)
+      console.log("new task added",json)
+    }
+  }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h3>Add a new Task</h3>
             <label>Task Title:</label>
             <input type="text" onChange={(e)=>setTitle(e.target.value)} value={title}/>
@@ -18,6 +44,8 @@ const TaskForm = () => {
             <input type="text" onChange={(e)=>setPriority(e.target.value)} value={priority}/>
             <label>Task Labels:</label>
             <input type="text" onChange={(e)=>setLabel(e.target.value)} value={label}/>
+            <button>Add Task</button>
+            {error && <div>{error}</div>}
         </form>  
   )
 }
