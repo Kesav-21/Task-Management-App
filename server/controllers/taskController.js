0808bf09af938b87c6782a6059a4a7a5bf9b5e82
Kesav-21 <cgkesav@gmail.com>
2023-07-15 08:20:52 +0530

    backend completed

---
+++ server/controllers/taskController.js
@@ -0,0 +1,80 @@
+const Task=require('../models/taskModel')
+const mongoose=require('mongoose')
+
+//get all tasks
+const getTasks=async(req,res)=>{
+    const tasks=await Task.find({}).sort({createdAt:-1})
+    res.status(200).json(tasks)
+}
+
+// get a single task
+const getTask=async(req,res)=>{
+    const {id}=req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"Not a Valid id"})
+    }
+
+    const task=await Task.findById(id)
+
+    if(!task){
+        return res.status(404).json({error:"No such Task"})
+    }
+
+    res.status(200).json(task)
+}
+
+//create a new task
+const createTask=async(req,res)=>{
+    const {title,due,priority,label}=req.body
+
+    try{
+        const task=await Task.create({title,due,priority,label})
+        res.status(200).json(task)
+    }catch(error){
+        res.status(400).json({error:error.message})
+    }
+}
+
+//delete a single task
+const deleteTask=async(req,res)=>{
+    const {id}=req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:'Not a valid'})
+    }
+
+    const task=await Task.findOneAndDelete({_id:id})
+
+    if(!task){
+        return res.status(400).json({error:"Not found"})
+    }
+
+    res.status(200).json(task)
+}
+
+//patch a single task
+const updateTask=async (req,res)=>{
+    const {id}=req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:'Not a valid'})
+    }
+
+    const task=await Task.findByIdAndUpdate({_id:id},{
+        ...req.body
+    })
+    
+    if(!task){
+        return res.status(400).json({error:"Not found"})
+    }
+
+    res.status(200).json(task)
+}
+
+
+module.exports={
+    createTask,
+    getTask,
+    getTasks,
+    deleteTask,
+    updateTask
+}
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,38 +1,27 @@
-const express=require('express');
-const Task=require('../models/taskModel')
+const express=require('express')
+const {createTask,
+    getTasks,
+    getTask,
+    deleteTask,
+    updateTask}=require('../controllers/taskController')
+
+
 const router=express.Router()
 
 // Get all tasks
-router.get('/',(req,res)=>{
-    res.json({mes:"Hello tasks"})
-})
+router.get('/',getTasks)
 
 //Get a task
-router.get('/:id',(req,res)=>{
-    res.json({mes:"get task id"})
-})
+router.get('/:id',getTask)
 
 //Post a new task
-router.post('/',async (req,res)=>{
-    const {title,due,priority,label}=req.body
-    console.log(req.body.label)
-    try{
-        const task=await Task.create({title,due,priority,label})
-        res.status(200).json(task)
-    }catch(error){
-        res.status(400).json({error:error.message})
-    }
-})
+router.post('/',createTask)
 
 //Delete a task
-router.delete('/:id',(req,res)=>{
-    res.json({msg:"delete new workout"})
-})
+router.delete('/:id',deleteTask)
 
 //Update a task
-router.patch('/:id',(req,res)=>{
-    res.json({msg:"Patch new workout"})
-})
+router.patch('/:id',updateTask)
 
 module.exports=router
 
