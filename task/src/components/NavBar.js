6b6e97055953a9b8001b089b14c8acdd45146f4a
Kesav-21 <cgkesav@gmail.com>
2023-07-19 20:28:09 +0530

    Logout

--- task/src/components/NavBar.js
+++ task/src/components/NavBar.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useLogout} from '../hooks/useLogout'
 
 const NavBar = () => {
+  const {logout}=useLogout()
+
+  const handleClick=()=>{
+    logout()
+  }
+  
   return (
     <header>
         <div className='header-container'>
@@ -9,6 +16,9 @@ const NavBar = () => {
                 <h1>Task Management App</h1>
             </Link>
             <nav>
+              <div>
+                <button onClick={handleClick}>LOGOUT</button>
+              </div>
               <div>
                 <Link to='/login'>Login</Link>
                 <Link to='/register'>Register</Link>
