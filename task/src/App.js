0ea880ffb3d875cb3e0e59e5abfe1b0409bba9db
Kesav-21 <cgkesav@gmail.com>
2023-07-19 21:59:40 +0530

    React Routes Auth

--- task/src/App.js
+++ task/src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const {user}=useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
       <NavBar/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
+        <Route path='/' element={user? <Home/>:<Navigate to="/login"/>}/>
+        <Route path='/login' element={!user? <Login/>:<Navigate to="/"/>}/>
+        <Route path='/register' element={!user? <Register/>:<Navigate to="/"/>}/>
       </Routes>
       </BrowserRouter>
     </div>
