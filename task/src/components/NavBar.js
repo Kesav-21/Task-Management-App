78286c4795e3c8203e596eeb8c34abf1e84eeb99
Kesav-21 <cgkesav@gmail.com>
2023-07-19 21:11:35 +0530

    Auth Context and Login

--- task/src/components/NavBar.js
+++ task/src/components/NavBar.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {useLogout} from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const NavBar = () => {
   const {logout}=useLogout()
+  const {user}=useAuthContext()
 
   const handleClick=()=>{
     logout()
   }
-  
+
   return (
     <header>
         <div className='header-container'>
@@ -16,9 +18,10 @@ const NavBar = () => {
                 <h1>Task Management App</h1>
             </Link>
             <nav>
-              <div>
+              {user && (<div>
+                <span>{user.email}</span>
                 <button onClick={handleClick}>LOGOUT</button>
-              </div>
+              </div>)}
               <div>
                 <Link to='/login'>Login</Link>
                 <Link to='/register'>Register</Link>
--- task/src/pages/Login.js
+++ task/src/pages/Login.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 const Login = () => {
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
+    const {login,error,isLoading}=useLogin()
 
-    const handleSubmit=(e)=>{
+    const handleSubmit=async(e)=>{
         e.preventDefault();
-        console.log(email,password)
+        await login(email,password)
     }
 
   return (
@@ -20,7 +22,8 @@ const Login = () => {
         <input type="password" onChange={(e)=>setPassword(e.target.value)}
         value={password}
         />
-        <button>Login</button>
+        <button disabled={isLoading}>Login</button>
+        {error && <div>{error}</div>}
 
     </form>
   )
